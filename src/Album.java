52684441ae7cf1115de6348c6f47abc87205ed56
kakacordovil <javanaoecafe@gmail.com>
2019-12-09 23:43:59 +0000

    Initial commit

---
+++ src/Album.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Album {
+    LinkedList <Album> songs = new LinkedList<>();
+
+    public static void addInOrder(LinkedList<Album> songs){
+
+    }
+}
---
+++ src/Song.java
@@ -0,0 +1,4 @@
+public class Song {
+    String title;
+    double duration;
+}
